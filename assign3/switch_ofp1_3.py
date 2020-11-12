ab6debc6b569531e24d705e85f2bd56b78421e70
rxkt <ericliao10797@hotmail.com>
2020-11-12 00:09:53 -0500

    cleaned code, fixed incorrect shortest_paths

--- assign3/switch_ofp1_3.py
+++ assign3/switch_ofp1_3.py
@@ -5,17 +5,28 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
 from ryu.lib.packet import packet, ethernet
 from ryu.app.ofctl.api import get_datapath
+from ryu.topology.api import get_switch, get_link
+from ryu.topology import event
+from ryu.lib import hub
 
 class Switch(app_manager.RyuApp):
     OFP_VERSIONS =[ofproto_v1_3.OFP_VERSION]
 
+    
+    
     def __init__(self, *args, **kwargs):
         super(Switch, self).__init__(*args, **kwargs)
         self.MAC_table = {}
         # for assignment 2
         self.ARP_table = {}
         self.shortest_paths = {}
+        self.switches = []
 
+    def remove_MAC(self, mac):
+        for key in self.MAC_table:
+            self.MAC_table.pop(mac)
+        
+        
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, ev):
         msg = ev.msg
@@ -33,10 +44,11 @@ class Switch(app_manager.RyuApp):
         #Create the MAC table for swid
         self.MAC_table.setdefault(swid,{})
 
-        #Learn Src. MAC
+        #Learn Src. MAC, avoid flood
         self.MAC_table[swid][smac] = pin
 
-        # MAC table lookup process
+        # if dest MAC is already avail, figure out which port to output
+        # otherwise flood, but dont flood?
         if dmac in self.MAC_table[swid]:
             port_out = self.MAC_table[swid][dmac]
         else:
@@ -52,11 +64,10 @@ class Switch(app_manager.RyuApp):
             datapath=dp, buffer_id=msg.buffer_id, 
             in_port=pin, actions=actions, 
             data=data)
+        
         dp.send_msg(out)
 
-        # prepare and send FLOW MOD (add new enty to the OFS)
-        # 1.1: here is match, idle_timeout: 10s. (1/23 on ryudocs)
-        # we put into mod=OFPFlowMod(..., idle_timeout=10, ...)
+        # 1.1: idle_timeout: 10s for our flowmod
         inst = [ofp_parser.OFPInstructionActions(
             ofp.OFPIT_APPLY_ACTIONS, actions)]
         match = ofp_parser.OFPMatch(
@@ -85,6 +96,14 @@ class Switch(app_manager.RyuApp):
 
         dp.send_msg(mod)
 
+    def remove_flow(self, datapath, match):
+        ofp = datapath.ofproto
+        ofp_parser = datapath.ofproto_parser
+        mod = ofp_parser.OFPFlowMod(datapath=datapath, command=ofp.OFPFC_DELETE, out_port=ofp.OFPP_ANY,
+                                    out_group=ofp.OFPP_ANY, match=match)
+        print('Removed flow of datapath id: {}, match: dest MAC: {}'.format(datapath.id, match['eth_dst']))
+        datapath.send_msg(mod)
+        
     # a simple get dict key by value function
     def get_MAC(self, table, port):
         for key in table:
@@ -92,81 +111,84 @@ class Switch(app_manager.RyuApp):
                 return key
         return None
 
-    # 1.2 : host removed from network; update mac table
     @set_ev_cls(ofp_event.EventOFPPortStatus, MAIN_DISPATCHER)
-    def port_status_hander(self, ev):
+    def port_status_handler(self, ev):
         msg = ev.msg
         dp = msg.datapath
         ofp = dp.ofproto
         ofp_parser = dp.ofproto_parser
         swid = dp.id
         port = msg.desc
-
-        print('Port state: {}'.format(port.state))
         
         if port.state == ofp.OFPPS_LINK_DOWN:
-            # remove invalid entries here from MAC address table
+            # 1.2 : host removed from network; update mac table
             bad_MAC = self.get_MAC(self.MAC_table[swid], port.port_no)
             if bad_MAC != None:
                 self.MAC_table[swid].pop(bad_MAC)
-                print('Removed from MAC table: DEST MAC: {}'.format(bad_MAC))
+                print('Removed from MAC table: MAC: {}'.format(bad_MAC))
             
-            # 1.3: 
-            # all flow entries related to that host should be removed;
-            # hint: OFPFC_DELETE
+            # 1.3: all flow entries related to that host should be removed;
             match = ofp_parser.OFPMatch(eth_dst=bad_MAC)
             dp_list = get_datapath(self, None)
-            for dp_item in dp_list:                
-                mod = ofp_parser.OFPFlowMod(datapath=dp_item, command=ofp.OFPFC_DELETE, out_port=ofp.OFPP_ANY,
-                                            out_group=ofp.OFPP_ANY, match=match)
-                print('Removed flow of datapath id: {}, match: dest MAC: {}'.format(dp_item.id, match['eth_dst']))
-                dp_item.send_msg(mod)
+            for dp_item in dp_list:
+                self.remove_flow(dp_item, match)
                 
-        # print('shortest path test: ', self.get_shortest_path(1,2))
-                
+        # triggering the test with deleting a host
+        # print('Shortest paths test: ', self.get_shortest_paths())
+        
+    # 2.1 : Find best routes between all pairs of OFS:
+    def get_shortest_paths(self):
+        topo_raw_switches = get_switch(self, None)
+        topo_raw_links = get_link(self, None)
+
+        links = {}
+        for switch in topo_raw_switches:
+            links[switch.dp.id] = []
+        
+        for link in topo_raw_links:
+            # link.src.dpid, link.dst.dpid
+            if link.dst.dpid not in links[link.src.dpid]:
+                links[link.src.dpid].append(link.dst.dpid)
+            if link.src.dpid not in links[link.dst.dpid]:
+                links[link.dst.dpid].append(link.src.dpid)
+        switches = [switch.dp.id for switch in topo_raw_switches]
+        
+        for i in switches: 
+            self.shortest_paths[i] = {}
+        for i in switches:
+            for j in switches:
+                if i != j:
+                    self.get_shortest_path(i, j, links)
+
+        return self.shortest_paths
+        
+          
     # 2.1 : Find best route between pair of OFS:
-    def get_shortest_path(self, swid1, swid2):
+    def get_shortest_path(self, swid1, swid2, links):
         '''return the shortest path from swid1 to swid2. '''
 
-        # we will probably want to use dynamic programming and save the shortest path.
-        # note shortest_path of swid1 -> swid2 is the reverse of swid2 -> swid1
+        # exit early + get saved data if it already exists
+        if swid2 in self.shortest_paths[swid1]:
+            return self.shortest_paths[swid1][swid2]
 
-        # get saved shortest path. current commented out due to issues with integer keys for dict
-        # if swid1 in self.shortest_paths and swid2 in self.shortest_paths[swid1]:
-            # return self.shortest_paths[swid1][swid2]
-
-        # apply BFS to determine shortest path
-        end_MAC = self.get_MAC(self.MAC_table[swid2], 1)
-        # queue contains all paths
-        queue = [[swid1]]
-        dp_list = get_datapath(self, None)
-        # list of available swids in the network
-        swids = [dp_item.id for dp_item in dp_list]
-        # a dict to translate MACs->swids
-        MACs = {}
-        for swid in swids:
-            MACs[self.get_MAC(self.MAC_table[swid], 1)] = swid
         visited = []
+        queue = [[swid1]]
         while len(queue) > 0:
             path = queue.pop(0)
             swid = path[-1]
             if swid not in visited:
-                neighbors = self.MAC_table[swid]
+                neighbors = links[swid]
                 for neighbor in neighbors:
-                    # skip non-switch
-                    if neighbor not in MACs:
-                        continue
-                    neighbor_id = MACs[neighbor]
                     new_path = list(path)
-                    new_path.append(neighbor_id)
+                    new_path.append(neighbor)
                     queue.append(new_path)
-                    if neighbor_id == swid2:
-                        # save shortest paths
-                        # some issue with int key for dict, remove cache thing for now
-                        # self.shortest_paths[swid1][swid2] = new_path
-                        # self.shortest_paths[swid2][swid1] = list(new_path).reverse()
+                    if neighbor == swid2:
+                        self.shortest_paths[swid1][swid2] = new_path
+                        self.shortest_paths[swid2][swid1] = new_path[::-1]
                         return new_path
                 visited.append(swid)
-        print('MAC table: ', self.MAC_table)
         print ('Unable to find path from swid {} to swid {}'.format(swid1, swid2))
+        return None
+
+        
         
